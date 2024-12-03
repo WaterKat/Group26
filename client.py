8278da94317bf26f90ee5569a92e1610008462f2
aguerr404 <alexisguerr404@gmail.com>
2024-12-03 01:04:39 -0800

    Add query input loop for client

--- client.py
+++ client.py
@@ -19,15 +19,28 @@ while True:
         continue
 
 # Keep accepting inputs until error
-while True:
-    message = input("Message: ")
-    try:
-        # Send message to server and receive
-        TCPsocket.send(message.encode())
-        print("Received from server: ", TCPsocket.recv(1024).decode())
-    except Exception as e:
-        print("Error sending message: ", e)
-        print()
+choice = "0"
+while int(choice) != 4:
+    choice = input("\nChoose a query (1-3) or Cancel (4):\n"
+                   "  1. What is the average moisture inside my kitchen fridge for the past 3 hours?\n"
+                   "  2. What is the average water consumption per cycle in my smart dishwasher?\n"
+                   "  3. Which device consumed more electricity among my three IoT devices?\n"
+                   "  4. Cancel\n")
+    if int(choice) == 4:
+        print("Closing connection.")
         break
+    elif int(choice) not in range(1, 4):
+        print("Sorry, that is not a valid query choice. Please try again.\n")
+        continue
+    # try:
+    # Send message to server and receive
+    # TCPsocket.send(choice.encode())
+    # print("Received from server: ", TCPsocket.recv(1024).decode())
+    # TEST TODO: delete when input loop works
+    print("Sending to server...")
+    # except Exception as e:
+    #     print("Error sending message: ", e)
+    #     print()
+    #     break
 
 TCPsocket.close()
