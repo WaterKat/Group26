8aef53fb89d0f25b9201106ed32ff7366f482533
WaterKat <waterkat@aonyxlimited.com>
2024-12-10 17:21:07 -0800

    Updating mongodb queries to use device uid's instead of device type uid

--- server.py
+++ server.py
@@ -10,20 +10,26 @@ load_dotenv()
 tcp_socket = socket.socket()
 max_socket_timeouts = 60
 
+
 def LitersPerMinuteToGallonsPerMinute(lpm):
     return lpm / 3.78541
+
 
 def MoistureToPercent(moisture):
     return moisture * 100
 
+
 def ConvertCtoF(celsius):
-    return celsius * 9/5 + 32
+    return celsius * 9 / 5 + 32
+
 
 def AmpsToWatts(amps, volts, power_factor):
     return amps * volts * power_factor
 
+
 def WattsToKilowattHours(watts, hours):
     return watts * hours / 1000
+
 
 queries = [
     "What is the average moisture inside my kitchen fridge in the past three hours?",
@@ -37,79 +43,113 @@ rejection = (
 
 appliance_map = {"fr-1": "fridge 1", "fr-2": "fridge 2", "dw-1": "dishwasher"}
 
-query1Query = [
-    {
-        "$project": {
-            "timeSince": {
-                "$dateDiff": {
-                    "startDate": "$time",
-                    "endDate": "$$NOW",
-                    "unit": "hour",
+
+def GetQueryOne(instanceUid, timeDifference=3):
+    return [
+        {
+            "$project": {
+                "instanceUid": "$payload.asset_uid",
+                "hygrometer": "$payload.fr-1-hygrometer",
+                "time": {
+                    "$dateDiff": {
+                        "startDate": "$time",
+                        "endDate": "$$NOW",
+                        "unit": "hour",
+                    }
                 },
-            },
-            "hygrometer": {
-                "$toDouble": "$payload.fr-1-hygrometer",
-            },
+            }
         },
-    },
+        {
+            "$match": {
+                "instanceUid": instanceUid,
+                "hygrometer": {"$exists": "true", "$ne": "null"},
+                "time": {"$lte": timeDifference},
+            }
+        },
+        {
+            "$group": {
+                "_id": "$instanceUid",
+                "hygrometer": {"$avg": {"$toDouble": "$hygrometer"}},
+            }
+        },
+    ]
+
+
+def GetQueryTwo(instanceUid):
+    return [
+        {"$match": {"payload.asset_uid": instanceUid}},
+        {
+            "$group": {
+                "_id": "$payload.asset_uid",
+                "flow": {"$avg": {"$toDouble": "$payload.dw-1-flowmeter"}},
+            }
+        },
+    ]
+
+def GetQueryThree():
+    return [
     {
-        "$match": {
-            "hygrometer": {
-                "$exists": "true",
-                "$ne": "null",
-            },
-            "timeSince": {
-                "$lte": 3,
-            },
-        },
-    },
-    {
-        "$group": {
-            "_id": "null",
-            "avg-hygrometer": {
-                "$avg": "$hygrometer",
-            },
-        },
-    },
+        '$project': {
+            'uid': '$payload.asset_uid', 
+            'current': {
+                '$switch': {
+                    'branches': [
+                        {
+                            'case': {
+                                '$eq': [
+                                    '$payload.parent_asset_uid', 'fr-1'
+                                ]
+                            }, 
+                            'then': '$payload.fr-1-ammeter'
+                        }, {
+                            'case': {
+                                '$eq': [
+                                    '$payload.parent_asset_uid', 'fr-2'
+                                ]
+                            }, 
+                            'then': '$payload.fr-2-ammeter'
+                        }, {
+                            'case': {
+                                '$eq': [
+                                    '$payload.parent_asset_uid', 'dw-1'
+                                ]
+                            }, 
+                            'then': '$payload.dw-1-ammeter'
+                        }
+                    ], 
+                    'default': 'null'
+                }
+            }
+        }
+    }, {
+        '$group': {
+            '_id': '$uid', 
+            'current': {
+                '$max': '$current'
+            }
+        }
+    }, {
+        '$sort': {
+            'current': -1
+        }
+    }
 ]
 
-query2Query = [
-    {"$match": {"payload.dw-1-flowmeter": {"$exists": True}}},
+meta_query = [
     {
-        "$group": {
-            "_id": None,
-            "flow": {"$avg": {"$toDouble": "$payload.dw-1-flowmeter"}},
+        "$unwind": {
+            "path": "$customAttributes.children",
+            "includeArrayIndex": "array_index",
+            "preserveNullAndEmptyArrays": False,
         }
     },
-]
-
-query3Query = [
     {
         "$project": {
-            "uid": "$payload.parent_asset_uid",
-            "current": {
-                "$switch": {
-                    "branches": [
-                        {
-                            "case": {"$eq": ["$payload.parent_asset_uid", "fr-1"]},
-                            "then": "$payload.fr-1-ammeter",
-                        },
-                        {
-                            "case": {"$eq": ["$payload.parent_asset_uid", "fr-2"]},
-                            "then": "$payload.fr-2-ammeter",
-                        },
-                        {
-                            "case": {"$eq": ["$payload.parent_asset_uid", "dw-1"]},
-                            "then": "$payload.dw-1-ammeter",
-                        },
-                    ],
-                    "default": "null",
-                }
-            },
+            "assetUid": "$assetUid",
+            "instanceUid": "$customAttributes.children.assetUid",
+            "name": "$customAttributes.name",
         }
     },
-    {"$group": {"_id": "$uid", "current": {"$max": "$current"}}},
-    {"$sort": {"current": -1}},
 ]
 
 print("starting...")
@@ -164,19 +204,31 @@ while True:
                 client_socket.send(rejection.encode())
                 continue
             # TODO: process the query and send the result back to the client
-            col = mongodb_client.get_database("dataniz").get_collection(
+            data_collection = mongodb_client.get_database("dataniz").get_collection(
                 "general_virtual"
             )
-            metadata = mongodb_client.get_database("dataniz").get_collection("general_metadata")
+            metadata_collection = mongodb_client.get_database("dataniz").get_collection(
+                "general_metadata"
+            )
+            devices = list(metadata_collection.aggregate(meta_query))
             if message == queries[0]:
-                aggr_result = col.aggregate(query1Query)
+                possible_device = next(
+                    (i for i in devices if i["assetUid"] == "fr-1"), None
+                )
+                if possible_device == None:
+                    response_string = "No device found"
+                    print(f"sending response: {response_string}")
+                    client_socket.send(response_string.encode())
+                    continue
+                fridge_uid = possible_device["instanceUid"]
+                aggr_result = data_collection.aggregate(GetQueryOne(fridge_uid))
                 aggr_list = list(aggr_result)
                 if len(aggr_list) == 0:
                     response_string = "No data found in the past three hours"
                     print(f"sending response: {response_string}")
                     client_socket.send(response_string.encode())
                     continue
-                reading = aggr_list[0]["avg-hygrometer"]/100
+                reading = aggr_list[0]["hygrometer"] / 100
                 moisture_percentage = MoistureToPercent(reading)
                 if moisture_percentage is None:
                     moisture_percentage = 0
@@ -184,11 +236,20 @@ while True:
                     moisture_percentage = 0
                 if moisture_percentage > 100:
                     moisture_percentage = 100
-                response_string = f"The average moisture inside your kitchen fridge in the past three hours is {int(moisture_percentage)}%"
+                response_string = f"The average moisture inside your kitchen fridge {possible_device["name"]} in the past three hours is {int(moisture_percentage)}%"
                 print(f"sending response: {response_string}")
                 client_socket.send(response_string.encode())
             elif message == queries[1]:
-                aggr_result = col.aggregate(query2Query)
+                possible_device = next(
+                    (i for i in devices if i["assetUid"] == "dw-1"), None
+                )
+                if possible_device == None:
+                    response_string = "No device found"
+                    print(f"sending response: {response_string}")
+                    client_socket.send(response_string.encode())
+                    continue
+                instanceUid = possible_device["instanceUid"]
+                aggr_result = data_collection.aggregate(GetQueryTwo(instanceUid))
                 aggr_list = list(aggr_result)
                 if len(aggr_list) == 0:
                     response_string = "No data found"
@@ -198,29 +259,28 @@ while True:
                 moisture_percentage = aggr_list[0]["flow"]
                 cycle_hours = 2
                 gpm = LitersPerMinuteToGallonsPerMinute(moisture_percentage)
-                response_string = f"The average water consumption per cycle in your smart dishwasher is {moisture_percentage:.1f} gpm"
+                response_string = f"The average water consumption per cycle in your smart dishwasher {possible_device["name"]} is {moisture_percentage:.1f} gpm"
                 print(f"sending response: {response_string}")
                 client_socket.send(response_string.encode())
             elif message == queries[2]:
-                aggr_result = col.aggregate(query3Query)
+                aggr_result = data_collection.aggregate(GetQueryThree())
                 aggr_list = list(aggr_result)
                 if len(aggr_list) == 0:
                     response_string = "No data found"
                     print(f"sending response: {response_string}")
                     client_socket.send(response_string.encode())
                     continue
-                uid = aggr_list[0]["_id"]
+                instanceUid = aggr_list[0]["_id"]
                 amps = float(aggr_list[0]["current"])
                 watts = AmpsToWatts(amps, 120, 1)
                 wHours = WattsToKilowattHours(watts, 1.5)
-                device = metadata.find_one({"assetUid": uid})
-                if device is None:
+                matchingDevice = next((i for i in devices if i["instanceUid"] == instanceUid), None)
+                if matchingDevice is None:
                     response_string = "No device found"
                     print(f"sending response: {response_string}")
                     client_socket.send(response_string.encode())
                     continue
-                appliance = device["customAttributes"]["name"]
-                response_string = f"The device that consumed more electricity among your three IoT devices is {appliance} with {wHours:.1f} kWh"
+                response_string = f"The device that consumed more electricity among your three IoT devices is {matchingDevice["name"]} with {wHours:.1f} kWh"
                 print(f"sending response: {response_string}")
                 client_socket.send(response_string.encode())
             else:
@@ -244,7 +304,3 @@ while True:
     print("connection closed with: ", client_ip)
     print("waiting for new connection...")
     print("listening on host: ", serverIP, ", port: ", serverPort)
-
-
-# { "payload.parent_asset_uid": "fr-1" }
-# { "avg-hygrometer" : { "$avg": "$payload.fr-1-hygrometer" } }
